279dc1c78a5f0974c577092d6ba432c563f2ae4b
RDCoder8 <mojinr@gmail.com>
2023-09-09 14:09:38 -0400

    Create Route completed. Able to add pokemon.

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 const pokemon = require('./models/pokemon')
+const mongoose = require('mongoose')
 
+
+///Engine
 app.set("view engine", "jsx")
 const jsxViewEngine = require('jsx-view-engine')
 app.engine("jsx", jsxViewEngine())
 
+//Middleware
+app.use(express.urlencoded({extended: false}))
+
+//Home Page
 app.get("/", (req, res) => {
     res.send("Welcome to the Pokemon App!")
 })
@@ -20,6 +28,18 @@ app.get("/pokemon", (req, res) => {
 app.get("/pokemon/new", (req, res) => {
     res.render("New")
 })
+
+//Delete
+
+//Update
+
+//Create
+app.post('/pokemon', (req, res) => {
+    pokemon.push(req.body)
+    res.send("Data sent")
+})
+
+//Edit
 
 //Show
 app.get("/pokemon/:id", (req, res) => {
